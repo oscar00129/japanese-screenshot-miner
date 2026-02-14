c4cc198916cbcbfa603890cd86541dfc9c366385
Oscar Castillo <oscar00129@gmail.com>
2026-02-14 14:44:11 -0700

    Added config class

---
+++ config/config.py
@@ -0,0 +1,20 @@
+import tomllib
+from pathlib import Path
+
+from generator.generator import Generator as g
+
+class Config:
+    def __init__(self):
+        self.read_config_file()
+    
+    def read_config_file(self):
+        try:
+            with open("config.toml", "rb") as f:
+                config = tomllib.load(f)
+                self.screnshot_folder_names = config.get('screenshot_folders', [])
+                self.screnshot_folders = [Path(folder_name) for folder_name in self.screnshot_folder_names]
+
+        except FileNotFoundError:
+            print("Config file not found. Generating...")
+            g.generate_config_file()
+            self.read_config_file()
