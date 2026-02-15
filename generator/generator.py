98108cfd3cfa8343b250c8502ed2c9f7d68bd983
Oscar Castillo <oscar00129@gmail.com>
2026-02-14 20:56:40 -0700

    Added generators and readers to anki image path

--- generator/generator.py
+++ generator/generator.py
@@ -5,6 +5,7 @@ class Generator:
     def generate_config_file():
         filename = "config.toml"
         with open(filename, "w") as f:
-            f.write("screenshot_folders = [\n\t\"\",\n]\n")
+            f.write("screenshot_folders = [\n\t\"\",\n]\n\n")
+            f.write("anki_img_folder = \"\"\n")
         os.chmod(filename, stat.S_IRUSR | stat.S_IWUSR)
         print("Config file generated. Please add a route of files.")
